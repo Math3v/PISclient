c31f5ae240c76c1f215b3a1e2367a09d0969602f
Matej Minárik <matej.minarik@agile-ict.com>
2016-03-13 14:44:49 +0100

    Patients CRUD in progress

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
+  'myApp.patients',
+  'myApp.view2'
+ ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  $routeProvider.otherwise({redirectTo: '/patients'});
+}]).
+constant('apiUrl', 'http://localhost:3000/api');
