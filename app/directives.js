3f68b5abe6091da154501eef6aec95103b512f3b
Matej Minárik <matej.minarik@agile-ict.com>
2016-03-16 12:18:21 +0100

    Element display based on auth in progress

--- app/directives.js
+++ app/directives.js
@@ -14,4 +14,17 @@ angular.module('myApp.directives', [])
 		templateUrl: 'users/user_directive.html',
 		replace: false
 	};
+}])
+
+.directive('access', ['permissionService', function(permissionService){
+	return {
+		restrict: 'A',
+		link: function($scope, iElm, iAttrs, controller) {
+			if( permissionService.canAccess( iAttrs.access ) === true ) {
+				iElm.removeClass('hidden');
+			} else {
+				iElm.addClass('hidden');
+			}
+		}
+	};
 }]);
--- app/services.js
+++ app/services.js
@@ -22,4 +22,45 @@ angular.module('myApp.services', [])
 		getUser: getUser,
 		delUser: delUser	
 	};
+}])
+
+.factory('permissionService', ['currentUserService', function(currentUserService){
+	
+	var currentUser = currentUserService.getUser();
+
+	function canAccess( action ) {
+		if( currentUser.role === 'admin') {
+			return true;
+		} else if( currentUser.role === 'patient' ) {
+			switch( action ) {
+				case 'add-visit': return true;
+				case 'show-visit': return true;
+				default: return false;
+			}
+		} else if( currentUser.role === 'doctor' ) {
+			switch( action ) {
+				case 'edit-visit': return true;
+				case 'show-visit': return true;
+				case 'add-commission': return true;
+				case 'edit-commission': return true;
+				case 'show-commission': return true;
+				case 'add-examination': return true;
+				case 'edit-examination': return true;
+				case 'show-examination': return true;
+				case 'add-patient': return true;
+				case 'edit-patient': return true;
+				case 'delete-patient': return true;
+				case 'show-patient': return true;
+				case 'edit-doctor': return true;
+				case 'show-doctor': return true;
+				default: return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	return {
+		canAccess: canAccess
+	};
 }]);
