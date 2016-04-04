0ae7e2556c990aee2f894f13e370e43907e029c0
Matej Minárik <matej.minarik@agile-ict.com>
2016-04-04 14:35:43 +0200

    Auto logout

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@
 angular.module('myApp', [
   'ngRoute',
   'ngCookies',
+  'ngIdle',
   'myApp.users',
   'myApp.login',
   'myApp.commisions',
@@ -15,13 +16,20 @@ angular.module('myApp', [
   'myApp.filters'
  ])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', 'IdleProvider', 'KeepaliveProvider', function($routeProvider, IdleProvider, KeepaliveProvider) {
+  IdleProvider.idle(60); // 1 minute idle
+  IdleProvider.timeout(30); // after 30 seconds idle, time the user out
+  KeepaliveProvider.interval(60); // 1 minute keep-alive ping
+
   $routeProvider.otherwise({redirectTo: '/login'});
 }])
 
 .constant('apiUrl', 'http://localhost:3000/api')
 
-.run(['$rootScope', 'currentUserService', '$location', function($rootScope, currentUserService, $location){
+.run(['$rootScope', 'currentUserService', '$location', 'Idle', function($rootScope, currentUserService, $location, Idle){
+
+  Idle.watch();
+
 	$rootScope.$on('$routeChangeStart', function(evnt, next) {
 		console.log( "Next ", next );
 
@@ -45,7 +53,24 @@ angular.module('myApp', [
 	})
 }])
 
-.controller('indexController', ['currentUserService', '$scope', '$location', function(currentUserService, $scope, $location){
+.controller('indexController', ['currentUserService', '$scope', '$location', 'Idle', function(currentUserService, $scope, $location, Idle){
+
+  $scope.$on('IdleTimeout', function() {
+    console.log( "IdleTimeout" );
+    $scope.logout();
+  });  
+  $scope.$on('IdleStart', function() {
+    console.log( "IdleStart" );
+  });  
+  $scope.$on('IdleEnd', function() {
+    console.log( "IdleEnd" );
+  });  
+  $scope.$on('IdleWarn', function() {
+    console.log( "IdleWarn" );
+  });  
+  $scope.$on('Keepalive', function() {
+    console.log( "Keepalive" );
+  });
   
   $scope.$on('permissionsChanged', function() {
     $scope.currentUser = currentUserService.getUser();
--- app/filters.js
+++ app/filters.js
@@ -7,10 +7,10 @@ angular.module('myApp.filters', [])
 			if( currentUser.role === 'admin' ) {
 				filtered.push( item );
 			}
-			if( item.hasOwnProperty('patient_id') === true && currentUser.id == item.patient_id ) {
+			else if( item.hasOwnProperty('patient_id') === true && currentUser.id == item.patient_id ) {
 				filtered.push( item );
 			}
-			if( item.hasOwnProperty('doctor_id') === true && currentUser.id == item.doctor_id ) {
+			else if( item.hasOwnProperty('doctor_id') === true && currentUser.id == item.doctor_id ) {
 				filtered.push( item );
 			}
 		});
