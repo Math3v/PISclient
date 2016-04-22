ea4f48537af1efe5627b778010760a8f1027530e
Matej Minárik <matej.minarik@agile-ict.com>
2016-04-22 08:32:45 +0200

    Visit page functionality

--- app/visits/visits.js
+++ app/visits/visits.js
@@ -14,7 +14,7 @@ angular.module('myApp.visits', [])
 	});
 }])
 
-.controller('visitsController', ['$scope', function($scope){
+.controller('visitsController', ['$scope', 'apiUrl', '$http', function($scope, apiUrl, $http){
 	
 	$scope.showNewModal = function() {
 		$scope.visit = {};
@@ -25,8 +25,34 @@ angular.module('myApp.visits', [])
 		$scope.visit = visit;
 		$( "#editVisit" ).modal('show');
 	}
-}])
 
+	$scope.newVisit = function(visit) {
+		visit.status = 'new';
+		console.log(visit);
+		$http({
+			method: 'POST',
+			url: apiUrl + '/Visits',
+			data: visit
+		}).then(function(response) {
+			console.log("SUccess");
+			$scope.loadVisits();
+		}, function(response) {
+			console.log( "Error ", response);
+		})
+	}
+
+	$scope.loadVisits = function() {
+		$http({
+			method: 'GET',
+			url: apiUrl + '/Visits'
+		}).then(function(response) {
+			$scope.visits = response.data;
+		}, function(err) {
+			console.log(err);
+		})	
+	}
+}])
+ /*
 document.addEventListener("DOMContentLoaded", function(event) { 
   setTimeout(function(){ 
     var elements_new = document.getElementsByClassName("new");
@@ -37,3 +63,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
       elements_confirm[i].setAttribute("src", "img/accept.png");
   }, 400);
 });
+*/
